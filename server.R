bd160ca9daaeecf91718158f8ab0bf31a7f5cd22
dxlw <dxlwan88@gmail.com>
2017-11-04 06:49:48 +0100

    Git Init

---
+++ server.R
@@ -0,0 +1,22 @@
+library(plotly)
+library(quantmod)
+library(TTR)
+library(tseries)
+library(forecast)
+library(glue)
+library(dplyr)
+
+library(shiny)
+
+shinyServer(function(input, output) {
+  
+  plot_ticker <- function(ticker){
+    
+    
+  }
+
+  output$distPlot <- renderPlot({
+    
+  })
+
+})
---
+++ ui.R
@@ -0,0 +1,18 @@
+library(shiny)
+
+shinyUI(fluidPage(
+
+  # Application title
+  titlePanel("Old Faithful Geyser Data"),
+
+  # Sidebar with a slider input for number of bins
+  sidebarLayout(
+    sidebarPanel(
+    ),
+
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("distPlot")
+    )
+  )
+))
